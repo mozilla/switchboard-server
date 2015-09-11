0e4bd47adc7e75356c2ba7f1c3b8046dbf41e330
Mark Finkle <mark.finkle@gmail.com>
2015-09-11 01:36:07 -0400

    Massive format and typo cleanup

--- htdocs/SwitchboardManager.php
+++ htdocs/SwitchboardManager.php
@@ -40,93 +40,89 @@ class SwitchboardManager {
     var $country;
     var $version;
     var $appId;
-    
+
     function __construct($params) {
-    	$this->uuid = $this->getArrayParam($params, 'uuid');
-    	$this->device = $this->getArrayParam($params, 'device');
-    	$this->manufacturer = $this->getArrayParam($params, 'manufacturer');
-    	$this->lang = $this->getArrayParam($params, 'lang');
-    	$this->version = $this->getArrayParam($params, 'version');
-    	$this->country = $this->getArrayParam($params, 'country');
-    	$this->appId = $this->getArrayParam($params, 'appId');
-    }    
-    
-    /** Returns true if the requesting user is within the defined bucket. 
+      $this->uuid = $this->getArrayParam($params, 'uuid');
+      $this->device = $this->getArrayParam($params, 'device');
+      $this->manufacturer = $this->getArrayParam($params, 'manufacturer');
+      $this->lang = $this->getArrayParam($params, 'lang');
+      $this->version = $this->getArrayParam($params, 'version');
+      $this->country = $this->getArrayParam($params, 'country');
+      $this->appId = $this->getArrayParam($params, 'appId');
+    }
+
+    /** Returns true if the requesting user is within the defined bucket.
       * $low is included and $high is excluded.
       */
     function isInBucket($low, $high) {
-    	$userBucket = $this->getUserBucket($this->uuid);
-    	if ($userBucket >= $low && $userBucket < $high) 
-    		return true;
-    	else
-    		return false;
+      $userBucket = $this->getUserBucket($this->uuid);
+      if ($userBucket >= $low && $userBucket < $high)
+        return true;
+      else
+        return false;
     }
-    
+
     /** The quickest way to generate an A/B test. Sets experiment to active when user is with the defined bucked.
       * $low including, $high excluding
       */
     function turnOnBucket($low, $high) {
-    	if(empty($this->uuid)) 
-    		return $this->inactiveExperimentReturnArray();
-    	
-    	//define buckets for experiment
-    	if($this->isInBucket($low, $high))
-    		return $this->activeExperimentReturnArray();
-    	else
-    		return $this->inactiveExperimentReturnArray();
+      if (empty($this->uuid))
+        return $this->inactiveExperimentReturnArray();
+
+      // define buckets for experiment
+      if ($this->isInBucket($low, $high))
+        return $this->activeExperimentReturnArray();
+      else
+        return $this->inactiveExperimentReturnArray();
     }
-    
+
     /**
       * Helps to manage multiple applications for A/B testing.
     */
     function isApplicationId($applicationId) {
-    	if($applicationId == $this->appId)
-    		return true;
-    	else
-    		return false;
+      if ($applicationId == $this->appId)
+        return true;
+      else
+        return false;
     }
-    
-    /** Gives a result array for an experiement the user is part of. Optional takes a array of values that can 
+
+    /** Gives a result array for an experiement the user is part of. Optional takes a array of values that can
       * be received at the client.
       */
     function activeExperimentReturnArray($values = null) {
-    	return array('isActive' => true,
-    				'values' => $values);		
+      return array('isActive' => true, 'values' => $values);
     }
-    
+
     /** Returns the result array when a user is not in the experiment */
     function inactiveExperimentReturnArray() {
-    	return array('isActive' => false,
-    				'values' => null);		
+      return array('isActive' => false, 'values' => null);
     }
-    
+
     /** Renders all experiment values as JSON string and returns it as result */
     function renderResultJson($resultArray) {
-    	header('Content-type: application/json');
-    	echo json_encode($resultArray);
+      header('Content-type: application/json');
+      echo json_encode($resultArray);
     }
-    
+
     /********************************************************************************/
-    /************** internal functions, not need for creat experiments **************/
+    /************** internal functions, not needed for create experiments ***********/
     /********************************************************************************/
-    
+
     /**
-      * Return the bucket number of the user. There a 100 possible buckets.
-      */  
+      * Return the bucket number of the user. There are 100 possible buckets.
+      */
     function getUserBucket() {
-    	$checksum = crc32($this->uuid);
-		  $bucket = $checksum % 100;    	
-    	return $bucket;
+      $checksum = crc32($this->uuid);
+      $bucket = $checksum % 100;
+      return $bucket;
     }
-    
+
     function getArrayParam($array, $paramName){
-    	if(isset($array[$paramName])) 
-    		return $array[$paramName];
-    	else
-    		return "";
+      if (isset($array[$paramName]))
+        return $array[$paramName];
+      else
+        return "";
     }
-
-
 };
 
 ?>
